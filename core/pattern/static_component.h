a1402211ce83b691d67e06bd05dce8a728f53fe5
ssaturn <john.minuk.yun@gmail.com>
2022-02-08 05:11:56 +0900

    core를 정적라이브러리로 분리하도록 하자 데이터 베이스랑 네트워크 관련 빼자

--- core/pattern/static_component.h
+++ core/pattern/static_component.h
@@ -41,7 +41,7 @@ namespace sst::pattern
 	class static_component_owner
 	{
 		static constexpr size_t SIZE = sizeof...(TypeOfComponents);
-		using TupleTy = std::tuple<TypeOfComponents ...>;
+		using tuple_ty = std::tuple<TypeOfComponents ...>;
 		std::array<std::unique_ptr<static_component<OwnerTy>>, SIZE> components_{};
 
 	public:
@@ -50,22 +50,20 @@ namespace sst::pattern
 			register_static_component(owner);
 		}
 
-		template <size_t I = 0>
-		typename std::enable_if_t<I == SIZE, void>
-			register_static_component([[maybe_unused]] OwnerTy* owner) {}
+		template <size_t I = 0> requires (I == SIZE)
+		void register_static_component([[maybe_unused]] OwnerTy* owner) {}
 
-		template <size_t I = 0>
-		typename std::enable_if_t<(I < SIZE), void>
-			register_static_component(OwnerTy* owner)
+		template <size_t I = 0> requires (I < SIZE)
+		void register_static_component(OwnerTy* owner)
 		{
-			components_[I] = std::make_unique<std::tuple_element_t<I, TupleTy>>(owner);
+			components_[I] = std::make_unique<std::tuple_element_t<I, tuple_ty>>(owner);
 			register_static_component<I + 1>(owner);
 		}
 
 		template<typename ComponentTy> requires std::derived_from<ComponentTy, static_component<OwnerTy>>
 		ComponentTy* get()
 		{
-			constexpr size_t index = trait_util::tuple_element_index<ComponentTy, TupleTy>::value;
+			constexpr size_t index = trait_util::tuple_element_index<ComponentTy, tuple_ty>::value;
 			return static_cast<ComponentTy*>(components_[index].get());
 		}
 	};
